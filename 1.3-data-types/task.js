2444d3b8eb19d2a4637951766ed7ce2010c58368
justgogame <63605888+justgogame@users.noreply.github.com>
2020-06-17 21:21:56 +0300

    Add files via upload

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,29 @@
+"use strict";
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    if(Number.isNaN(percent)){
+        return `Некорректный параметр`;
+    } else if(Number.isNaN(contribution)){
+        return `Некорректный параметр`;
+    } else if(Number.isNaN(amount)) {
+        return `Некорректный параметр`;
+    } else {
+        let loanBody = amount - contribution;
+        let paidMonths = ((date.getFullYear() - new Date().getFullYear()) * 12) + (date.getMonth() - new Date().getMonth());
+        let P = (percent / 100) / 12;
+        let perMonth = loanBody * (P + P / (((1 + P) ** paidMonths) - 1));
+        let totalAmount = parseFloat((perMonth * paidMonths).toFixed(2));
+        return totalAmount;
+    }
 }
 
+
+
+
+
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+    if (name.length < 1 || name == "null" || name == "undefined"){
+        return `Привет, мир! Меня зовут Аноним`;
+    } else {
+        return `Привет, мир! Меня зовут ${name}`;
+    }
 }
